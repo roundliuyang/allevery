3851aadcd1e15428e9f82ae0a3bf15232643c8f1
roundliuyang <61273589+roundliuyang@users.noreply.github.com>
2021-10-19 00:51:53 +0800

    stringUtils

---
+++ java-string/string-tools/apache-commons-lang-3/src/main/java/com/yly/allevery/java/string/ApacheCommonsLangTest.java
@@ -0,0 +1,23 @@
+package com.yly.allevery.java.string;
+
+//Apache Commons Lang 3 库支持操作 Java API 的核心类。这种支持包括处理字符串、数字、日期、并发、对象反射等的方法。
+//除了提供对该库的一般介绍之外，本教程还演示了用于操作 String 实例的 StringUtils 类的方法
+public class ApacheCommonsLangTest {
+
+
+    //Maven Dependency
+    //  <dependency>
+    //      <groupId>org.apache.commons</groupId>
+    //      <artifactId>commons-lang3</artifactId>
+    //      <version>3.12.0</version>
+    //  </dependency>
+
+    // StringUtils
+    //StringUtils 类提供了对字符串进行空安全操作的方法。
+
+
+    //---------------------------------------------------------------------------------------
+
+    // containsAny方法
+
+}
