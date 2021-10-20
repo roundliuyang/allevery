1f1bdd6d60a49dcf57c2537d1e055835e2ea2f67
roundliuyang <61273589+roundliuyang@users.noreply.github.com>
2021-10-20 10:30:36 +0800

    collectionUtils

--- libraries-apache-commons/src/test/java/com/yly/commons/lang3/CollectionUtilsTest.java
+++
@@ -1,8 +0,0 @@
-package com.yly.commons.lang3;
-
-/**
- * 简而言之，Apache CollectionUtils为常见操作提供了实用方法，这些方法涵盖了广泛的用例并有助于避免编写样板代码。
- * 该库面向较旧的 JVM 版本，因为目前 Java 8 的Stream API提供了类似的功能。
- */
-public class CollectionUtilsTest {
-}
--- libraries-apache-commons/src/test/java/com/yly/commons/lang3/StringUtils/StringUtilsTest.java
+++ libraries-apache-commons/src/test/java/com/yly/commons/lang3/StringUtils/StringUtilsTest.java
@@ -1,4 +1,4 @@
-package com.yly.commons.lang3;
+package com.yly.commons.lang3.StringUtils;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
