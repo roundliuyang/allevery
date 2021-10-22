8bcf78c81a50a1d4d33128d59f7454b3a817fe3c
roundliuyang <61273589+roundliuyang@users.noreply.github.com>
2021-10-23 00:13:03 +0800

    zz

--- springboot-rest/src/main/java/com/yly/requestresponsebody/ExamplePostController.java
+++ springboot-rest/src/main/java/com/yly/requestresponsebody/ExamplePostController.java
@@ -38,7 +38,7 @@ public class ExamplePostController {
 
     // @ResponseBody 注释告诉控制器返回的对象会自动序列化为 JSON 并传递回 HttpResponse 对象
     // 请记住，我们不需要使用 @ResponseBody 注释来注释 @RestController 注释的控制器，因为 Spring 默认情况下会这样做。
-    //  https://www.baeldung.com/spring-request-response-body            todo
+    //  https://www.baeldung.com/spring-request-response-body
 
     @PostMapping("/response")
     @ResponseBody
@@ -47,17 +47,38 @@ public class ExamplePostController {
         return new ResponseTransfer("Thanks For Posting!!!");
     }
 
+
+    // 当我们使用@ResponseBody注解时，我们仍然能够明确地设置我们方法返回的内容类型。
+    // 为此，我们可以使用@RequestMapping的 produces 属性。注意，像@PostMapping、@GetMapping等注解为该参数定义了别名。
+    // 在示例中，我们使用了 MediaType.APPLICATION_JSON_VALUE 常量。或者，我们可以直接使用 application/json。
     @PostMapping(value = "/content", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseTransfer postResponseJsonContent(@RequestBody LoginForm loginForm) {
         log.debug("POST received - serializing LoginForm: " + loginForm.getPassword() + " " + loginForm.getUsername());
         return new ResponseTransfer("JSON Content!");
     }
-
+    
+    // 接下来，让我们实现一个新的方法，映射到相同的/content路径，但返回XML内容
     @PostMapping(value = "/content", produces = MediaType.APPLICATION_XML_VALUE)
     @ResponseBody
     public ResponseTransfer postResponseXmlContent(@RequestBody LoginForm loginForm) {
         log.debug("POST received - serializing LoginForm: " + loginForm.getPassword() + " " + loginForm.getUsername());
         return new ResponseTransfer("XML Content!");
     }
+
+    // 现在，根据请求标头中发送的 Accept 参数的值，我们将得到不同的响应。
+    //让我们看看这个：
+//    curl -i \
+//            -H "Accept: application/json" \
+//            -H "Content-Type:application/json" \
+//            -X POST --data
+//  '{"username": "johnny", "password": "password"}' "https://localhost:8080/.../content"
+//    CURL命令返回一个JSON响应。
+//    HTTP/1.1 200
+//    Content-Type: application/json
+//    Transfer-Encoding: chunked
+//    Date: Thu, 20 Feb 2020 19:43:06 GMT
+//
+//    {"text":"JSON Content!"}
+//
 }
