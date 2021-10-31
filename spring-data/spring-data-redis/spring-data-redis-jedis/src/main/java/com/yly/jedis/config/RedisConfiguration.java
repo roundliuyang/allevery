db5bec026701b7c5c2856d9f9f92e3207d01d1c2
[liuyang.yuan@mycool.tv] <[mail.mycool.tv]>
2021-10-31 19:34:10 +0800

    jedis

--- spring-data/spring-data-redis/spring-data-redis-jedis/src/main/java/com/yly/jedis/config/RedisConfiguration.java
+++ spring-data/spring-data-redis/spring-data-redis-jedis/src/main/java/com/yly/jedis/config/RedisConfiguration.java
@@ -8,6 +8,22 @@ import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
+/*
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of("password"));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+    JedisConnectionFactory 设置主机方法已经不建议使用，因此需要使用RedisStandaloneConfiguration配置JedisConnectionFactory。我们也可以使用属性文件进行配置：
+    spring.redis.host=127.0.0.1
+    spring.redis.password=password
+    spring.redis.port=6379
+    1
+    2
+    3
+    我们也可以配置连接池相关属性。
+*/
 @Configuration
 public class RedisConfiguration {
 
