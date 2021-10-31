9432d661124a9ded66b5eb3a6aaaf145eef56c76
roundliuyang <61273589+roundliuyang@users.noreply.github.com>
2021-10-31 19:27:47 +0800

    jedis

---
+++ spring-data/spring-data-redis/spring-data-redis-jedis/src/main/java/com/yly/jedis/Application.java
@@ -0,0 +1,8 @@
+package com.yly.jedis;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableTransactionManagement
+public class Application {
+}
