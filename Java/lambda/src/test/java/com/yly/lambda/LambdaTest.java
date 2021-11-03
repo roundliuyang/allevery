0d4aab8015074d174b3050ecbeb5cb9dbeb97d83
[liuyang.yuan@mycool.tv] <[mail.mycool.tv]>
2021-11-04 00:52:56 +0800

    other

---
+++ Java/lambda/src/test/java/com/yly/lambda/LambdaTest.java
@@ -0,0 +1,10 @@
+package com.yly.lambda;
+
+/*
+首选标准功能接口
+函数式接口收集在java.util.function包中，可满足大多数开发人员为 lambda 表达式和方法引用提供目标类型的需求。
+这些接口中的每一个都是通用的和抽象的，这使得它们很容易适应几乎所有的 lambda 表达式。开发人员应该在创建新的功能接口之前探索这个包。
+ */
+public class LambdaTest {
+
+}
