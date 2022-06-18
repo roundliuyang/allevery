5990f7249d736abfa1791f4f21c5ff4924f8875d
yly <liuyang.yuan>
2022-06-18 19:01:17 +0800

    springcloud rabbitmq

---
+++ labx-10-spring-cloud-stream-rabbitmq/labx-10-sc-stream-rabbitmq-producer-demo/src/main/java/com/yly/springcloud/labx10/rabbitmqdemo/producerdemo/message/MySource.java
@@ -0,0 +1,11 @@
+package com.yly.springcloud.labx10.rabbitmqdemo.producerdemo.message;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface MySource {
+
+    @Output("demo01-output")
+    MessageChannel demo01Output();
+
+}
