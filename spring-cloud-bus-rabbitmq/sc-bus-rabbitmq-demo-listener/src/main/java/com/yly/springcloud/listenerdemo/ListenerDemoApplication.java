e33f2c66a90258f3638536aeaa6cf5bb554ff674
yly <2337352929@qq.com>
2022-07-17 21:25:11 +0800

    cloud-bus-event

---
+++ spring-cloud-bus-rabbitmq/sc-bus-rabbitmq-demo-listener/src/main/java/com/yly/springcloud/listenerdemo/ListenerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.yly.springcloud.listenerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
+
+@SpringBootApplication
+@RemoteApplicationEventScan
+public class ListenerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerDemoApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-bus-rabbitmq/sc-bus-rabbitmq-demo-listener/src/main/java/com/yly/springcloud/listenerdemo/listener/UserRegisterListener.java
@@ -0,0 +1,23 @@
+package com.yly.springcloud.listenerdemo.listener;
+
+
+import com.yly.springcloud.listenerdemo.event.UserRegisterEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 用户注册事件的监听器
+ */
+@Component
+public class UserRegisterListener implements ApplicationListener<UserRegisterEvent> {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public void onApplicationEvent(UserRegisterEvent event) {
+        logger.info("[onApplicationEvent][监听到用户({}) 注册]", event.getUsername());
+    }
+
+}
