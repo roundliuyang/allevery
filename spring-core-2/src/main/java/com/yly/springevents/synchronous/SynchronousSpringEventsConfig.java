622622a6cee3584751eda6476310612c18c6ba59
yly <2337352929@qq.com>
2022-11-25 19:51:14 +0800

    spring event

---
+++ spring-core-2/src/main/java/com/yly/springevents/synchronous/SynchronousSpringEventsConfig.java
@@ -0,0 +1,10 @@
+package com.yly.springevents.synchronous;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.yly.springevents.synchronous")
+public class SynchronousSpringEventsConfig {
+
+}
