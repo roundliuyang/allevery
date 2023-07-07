56de66dc25c466b9c10ae23e7be7695904615452
yly <2337352929@qq.com>
2023-07-07 10:50:20 +0800

    test

--- aop/spring-aop/src/test/java/com/yly/CustomAnnotationIntegrationTest.java
+++ aop/spring-aop/src/test/java/com/yly/CustomAnnotationIntegrationTest.java
@@ -1,7 +1,8 @@
 package com.yly;
 
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
